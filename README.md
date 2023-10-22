2368f62ee4a0a3788f8317ba0b364e3c763404f6
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2023-10-22 20:01:04 +0530

    3. Create Header Component

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Real State Website Using MERN Stack
 1. Install React.js, Tailwind CSS and create the first template
 2. Create Pages and Routes </br>
     2.1 npm i react-router-dom
+3. Create Header Component </br>
+    2.1 npm i react-icons
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,17 @@ import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<Signin />} />
-      <Route path="/sign-out" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
   </BrowserRouter>
 }
