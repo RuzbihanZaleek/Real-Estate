490746c7f59fd5f436dae6d54fa76ba160f5dd02
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2023-10-22 19:11:36 +0530

    2. Create Pages and Routes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Real-State
 Real State Website Using MERN Stack
+
+1. Install React.js, Tailwind CSS and create the first template
+2. Create Pages and Routes
+    2.1 npm i react-router-dom
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-out" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
 }
