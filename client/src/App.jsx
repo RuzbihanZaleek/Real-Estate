db7e62ca53aaad286edd7332b06c3e73d54987c3
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2023-11-03 18:33:46 +0530

    Pretier Formatter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,16 +8,18 @@ import SignIn from './pages/Signin';
 import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
-  return <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route element={<PrivateRoute />}>
-        <Route path="/profile" element={<Profile />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,11 +1,9 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
 
-//Outlet to show the children 
+//Outlet to show the children
 export default function PrivateRoute() {
-    const {currentUser} = useSelector((state) => state.user);
-    
-    return (
-        currentUser ? <Outlet /> : <Navigate to="/sign-in" />
-    )
+  const { currentUser } = useSelector((state) => state.user);
+
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
